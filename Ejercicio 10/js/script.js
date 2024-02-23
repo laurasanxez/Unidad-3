78fc65ffec3371386771f67fe8b4b4b527293afd
laurasanxez <151147518+laurasanxez@users.noreply.github.com>
2024-02-23 11:34:38 +0100

    Add files via upload

---
+++ Ejercicio 10/js/script.js
@@ -0,0 +1,8 @@
+function Array() {
+    let alumno= ["Macarena", "Jaime", "Hugo", "Jimena"];
+    let notas= [];
+    notas [0] = parseInt(prompt(" ¿Que nota le pones a " + alumno[0] + "?" ));
+    notas [1] = parseInt(prompt(" ¿Que nota le pones a " + alumno[1] + "?" ));
+    notas [2] = parseInt(prompt(" ¿Que nota le pones a " + alumno[2] + "?" ));
+    notas [3] = parseInt(prompt(" ¿Que nota le pones a " + alumno[3] + "?" ));
+}
